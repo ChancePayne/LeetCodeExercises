ea072ccc0eb6e19555f83bf101940d34c9c51a73
Chance <cmpayne5@gmail.com>
2022-12-21 14:19:36 -0800

    complete kth largest

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun printDebug(text: String) {
 fun main() {
     val start = System.currentTimeMillis()
 
-    LongestSubstring.execute()
+    FindKthLargest.execute()
 
     println((System.currentTimeMillis() - start))
 }
