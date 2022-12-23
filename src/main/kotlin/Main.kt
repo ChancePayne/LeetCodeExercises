cb43cd5c0209156a95978b334c2e0c14c32e07a5
Chance <cmpayne5@gmail.com>
2022-12-23 13:09:45 -0800

    complete TwoSum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun printDebug(text: String) {
 fun main() {
     val start = System.currentTimeMillis()
 
-    MaximumDepthTree.execute()
+    TwoSum.execute()
 
     println((System.currentTimeMillis() - start))
 }
