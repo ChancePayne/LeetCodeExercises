e9e68cc39b607520df1a2f1f9b8a5602abe143bd
Chance <cmpayne5@gmail.com>
2022-12-26 09:18:09 -0800

    complete BinaryTreeLowestCommonAncestor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun printDebug(text: String) {
 fun main() {
     val start = System.currentTimeMillis()
 
-    MinimumDepthTree.execute()
+    BinaryTreeLowestCommonAncestor.execute()
 
     println()
     println((System.currentTimeMillis() - start))
