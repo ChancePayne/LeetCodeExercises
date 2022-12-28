be841bb6c3ec6e4227b5050840afdc68c09370e0
Chance <cmpayne5@gmail.com>
2022-12-28 12:03:55 -0800

    complete MaximumSubarray and finish climbStairs

--- src/main/kotlin/ClimbStairs.kt
+++ src/main/kotlin/ClimbStairs.kt
@@ -1,24 +1,41 @@
+import kotlin.math.roundToInt
+
 // 70. climbing stairs
 
 class ClimbStairs {
     companion object {
         fun execute() {
             val solution = ClimbStairs()
-            println(solution.climbStairs(2))
-            println(solution.climbStairs(3))
-            println(solution.climbStairs(6))
-            println(solution.climbStairs(4))
-            println(solution.climbStairs(1))
+            println(solution.climbStairs(1) == 1)
+            println(solution.climbStairs(2) == 2)
+            println(solution.climbStairs(3) == 3)
+            println(solution.climbStairs(4) == 5)
+            println(solution.climbStairs(5) == 8)
+            println(solution.climbStairs(6) == 13)
+            println(solution.climbStairs(35) == 14930352)
+            println(solution.climbStairs(45) == 1836311903)
         }
     }
 
     fun climbStairs(n: Int): Int {
-        var numWays = 0
+        return directFibonacci(n)
+    }
 
-        numWays += n % 2 + 1
-        val numTwos = n / 2
-        numWays += Math.pow(2.0, numTwos.toDouble()).toInt() - 1
+    private fun stepFibonacci(n: Int): Int {
+        var numWays = 1
+        var prevNumWays = 0
+        var tempNumWays = 0
+
+        for (i in 1..n) {
+            tempNumWays = numWays
+            numWays += prevNumWays
+            prevNumWays = tempNumWays
+        }
 
         return numWays
+    }
+
+    private fun directFibonacci(n: Int): Int {
+        return Math.round(((Math.pow((Math.pow(5.0, 0.5) + 1) / 2, (n + 1).toDouble())) - Math.pow((Math.pow(5.0, 0.5) - 1) / 2, (n + 1).toDouble())) / Math.pow(5.0, 0.5)).toInt()
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun printDebug(text: String) {
 fun main() {
     val start = System.currentTimeMillis()
 
-    BuyAndSellStock.execute()
+    ClimbStairs.execute()
 
     println()
     println((System.currentTimeMillis() - start))
