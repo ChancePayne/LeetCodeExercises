19d43b59325dada3181dbf97087ca982e97b5ba0
Chance <cmpayne5@gmail.com>
2022-12-30 17:29:08 -0800

    add ProductArray and MissingNumber solutions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun printDebug(text: String) {
 fun main() {
     val start = System.currentTimeMillis()
 
-    DuplicateNumber.execute()
+    ProductArray.execute()
 
     println()
     println((System.currentTimeMillis() - start))
