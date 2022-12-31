48fea7be780f4911c7cdedad666756900f809bd8
Chance <cmpayne5@gmail.com>
2022-12-30 16:58:45 -0800

    complete DuplicateNumber

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun printDebug(text: String) {
 fun main() {
     val start = System.currentTimeMillis()
 
-    HouseRobber.execute()
+    DuplicateNumber.execute()
 
     println()
     println((System.currentTimeMillis() - start))
