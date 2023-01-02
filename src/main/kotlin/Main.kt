6dd13f613fdb27fa861b56232614ef6fe8670917
Chance <cmpayne5@gmail.com>
2023-01-02 08:46:57 -0800

    work on RotateImage

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun printDebug(text: String) {
 fun main() {
     val start = System.currentTimeMillis()
 
-    TopKFrequent.execute()
+    RotateImage.execute()
 
     println()
     println((System.currentTimeMillis() - start))
