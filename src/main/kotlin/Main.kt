b8267eba636c09c9fe56d5c24d9811c50bbd49c7
Chance <cmpayne5@gmail.com>
2023-01-09 11:20:56 -0800

    complete PalindromeNumber

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun printDebug(text: String) {
 fun main() {
     val start = System.currentTimeMillis()
 
-    MergeTwoSortedLists.execute()
+    PalindromeNumber.execute()
 
     println()
     println((System.currentTimeMillis() - start))
